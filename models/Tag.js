294acb18e5a05e7c99330feb860cfb87d9a00f2b
esgar <navaedgar36@yahoo.com.mx>
2022-05-08 11:09:22 -0500

    backup all files

---
+++ models/Tag.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
